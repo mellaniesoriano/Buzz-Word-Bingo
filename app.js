f862cd2f2584eae7163c646a63579798b4df480c
mellaniesoriano <msoriano808@gmail.com>
2017-07-13 20:58:05 -1000

    added reset route

--- app.js
+++ app.js
@@ -4,11 +4,14 @@ const app = express();
 const router = express.Router();
 const bodyParser = require('body-parser');
 const buzzwordsRoute = require('./routes/buzzwords.js');
+const helpers = require('./helpers.js');
+const resetRoute = require('./routes/reset.js');
 
 app.use(express.static('public'));
 app.use(bodyParser.json());
 
 app.use('/buzzwords', buzzwordsRoute);
+app.use('/reset', resetRoute);
 
 const server = app.listen(3000, () => {
   console.log(`Server running on port 3000`);
