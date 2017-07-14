91c64e59befdaca8b55b015376de29de7e617900
mellaniesoriano <msoriano808@gmail.com>
2017-07-13 15:45:06 -1000

    some changes

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const express = require('express');
 const app = express();
 const router = express.Router();
 const bodyParser = require('body-parser');
-const buzzwordsRoute = require('./buzzwords.js');
+const buzzwordsRoute = require('./routes/buzzwords.js');
 
 app.use(express.static('public'));
 app.use(bodyParser.urlencoded({extended : true}));
@@ -11,5 +11,5 @@ app.use(bodyParser.urlencoded({extended : true}));
 app.use('/buzzwords', buzzwordsRoute);
 
 const server = app.listen(3000, () => {
-  console.log(`Server running on ${host}, at port ${port}`);
+  console.log(`Server running on port 3000`);
 });
