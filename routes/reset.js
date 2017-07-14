0aea407c4745e7df4b36050279cb97a2e3c6484b
mellaniesoriano <msoriano808@gmail.com>
2017-07-13 21:13:18 -1000

    removed get

--- routes/reset.js
+++ routes/reset.js
@@ -5,9 +5,6 @@ const router = express.Router();
 const helpers = require('../helpers.js');
 
 router.route('/')
-.get((req, res) => {
-  res.send(helpers.allTheWords);
-})
 .post((req, res) => {
   helpers.reset(req, res);
 });
