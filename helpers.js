cc43be14df826475b1991c7d9c96f098426bef61
mellaniesoriano <msoriano808@gmail.com>
2017-07-13 19:30:10 -1000

    put function works

--- helpers.js
+++ helpers.js
@@ -4,16 +4,34 @@ const allTheWords = [];
 
 const postBody = (req, res) => {
   if (allTheWords.map((curWord) => {
-    return curWord.buzzWord;}).indexOf(req.body.buzzWord) > -1){
+    return curWord.buzzWord;}).indexOf(req.body.buzzWord) > -1 || allTheWords.length >= 5){
     res.send({"success" : false});
   } else {
     allTheWords.push({"buzzWord": req.body.buzzWord,
-      "points": parseInt(req.body.points)});
-    res.send({"success" : true});
+      "points": parseInt(req.body.points),
+      "heard": false,
+      "newScore": 0});
+    res.send({"success" : true,
+      "newScore" : 0});
+  }
+};
+
+const putBody = (req, res) => {
+  const buzzObj = allTheWords[allTheWords.map((curWord) => {
+    return curWord.buzzWord;}).indexOf(req.body.buzzWord)];
+
+  if (allTheWords.map((curWord) => {
+    return curWord.buzzWord;}).indexOf(req.body.buzzWord) < -1){
+    res.send({"success" : false});
+  } else {
+    buzzObj.heard = true;
+    buzzObj.newScore += buzzObj.points;
+    res.send({"success" : true, "newScore" : buzzObj.newScore});
   }
 };
 
 module.exports = {
   allTheWords,
-  postBody
+  postBody,
+  putBody
 };
