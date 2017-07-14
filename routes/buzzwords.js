a1f433e4a1274a33868c26aef3a27542794221fc
mellaniesoriano <msoriano808@gmail.com>
2017-07-13 18:33:15 -1000

    cleaned up file, required helpers.js file

--- routes/buzzwords.js
+++ routes/buzzwords.js
@@ -2,25 +2,15 @@
 
 const express = require('express');
 const router = express.Router();
+const helpers = require('../helpers.js');
 
 router.route('/')
 .get((req, res) => {
-  res.send(allTheWords);
+  res.send(helpers.allTheWords);
 })
 .post((req, res) => {
-  if (allTheWords.map((curWord) => {
-    return curWord.buzzWord;}).indexOf(req.body.buzzWord) > -1){
-    res.send({"success" : false});
-  } else {
-    allTheWords.push({"buzzWord": req.body.buzzWord,
-      "points": Number(req.body.points)});
-    console.log('chk points..', Number(req.body.points));
-    res.send({"success" : true});
-  }
+  helpers.postBody(req, res);
 });
-
-const allTheWords = [];
-const score = 0;
 
 
 module.exports = router;
